b07a5709ca2f76d402cd1c915870e0752fe5c2ea
c-mac88 <christianmcfarland@gmail.com>
2016-06-07 02:52:13 -0700

    newStuff

--- app/controller.js
+++ app/controller.js
@@ -18,9 +18,53 @@
         }
 
 
-        vm.FAQ = [{"question": "Does it cost anything to apply?", "answer": "No. there is no cost to apply.", "showing": false},
-                    {"question": "Test", "answer": "Test", "showing": false}
-        ]
+        vm.FAQ = [{ "question": "Does it cost anything to apply?", "answer": "No. there is no cost to apply.", "showing": false },
+            { "question": "How long does the process take?", "answer": "Once you submit your application at a live event, you will receive a response almost instantly. When approved, your new loan will be processed and finalized the same day.", "showing": false },
+            { "question": "What's my interest rate and term on my new loan?", "answer": "Your interest rate and term will vary based on the lender you're matched with.", "showing": false },
+            { "question": "I have bad credit. Can I still apply?", "answer": "Yes. Many lenders specialize in providing loans to people who receive various types of benefits such as Social Security, Retirement or Veteran's Benefits.", "showing": false },
+            { "question": "How is my private information protected?", "answer": "TestYour information is encrypted and securely transmitted to the lender, who then decides whether to fund your loan. ", "showing": false },
+            { "question": "I don't have an auto loan. Can you help me get one?", "answer": "Absolutely. We work with over 40 different local and nationwide lenders that will provide a program that fits your current financial situation.", "showing": false },
+            { "question": "What if I no longer want my car and would like to trade it in?", "answer": "If you would like to trade out of your car we will align you with one of our local dealer partners to find you the vehicle you love!", "showing": false },
+            { "question": "I have a lease. Can you still Fix My Loan?", "answer": "Yes. A lease is just another form of auto loan. We specialize in both conventional and lease financing. ", "showing": false }
+        ];
+
+        vm.reviews = [
+        {"title": "Super Service", 
+        "text": "My experience was very positive. The website was user friendly and fast. Although there was an error transferring the information on my page to the lender, they called before I could physically log in to their website. They told me exactly what to do and followed through with processing on the next business day exactly like they said they would. I will definitely use creditloan.com again.", 
+        "name":"Traci"},
+        {"title": "Greg is the best!", 
+        "text": "He worked with me to get out of my high car payment.", 
+        "name":"Ashley"},
+        {"title": "Truely great people.", 
+        "text": "These guys love what they do and you can tell.", 
+        "name":"MATT"},
+        {"title": "Simple", 
+        "text": "I waited to long to do this.  Should have done it sooner.", 
+        "name":"Megan"},
+        {"title": "Thank you Jordan.", 
+        "text": "Just when I though I couldn't keep my car Jordan came to the rescue.  I'm so glad he was able to help me", 
+        "name":""},
+        {"title": "Helpful", 
+        "text": "I was relieved to find that they are just regular people trying to help others.", 
+        "name":"Adam"},
+        {"title": "I feel educated", 
+        "text": "I didn't end up fixing my loan because I was so close to the end, but Josh really helped me understand the ins and outs of credit.", 
+        "name":"Anthony"},
+        {"title": "Fast", 
+        "text": "Quick process.", 
+        "name":"Rick"},
+        {"title": "This really works!", 
+        "text": "I really couldn't believe how easy this was.  I was kicking myself for not having done it sooner.", 
+        "name":"Jessica"},
+        {"title": "Payment went down $126!", 
+        "text": "The guys at FixMyLoan are car loan ninjas.", 
+        "name":"Patrick"},
+
+
+        ];
+
+
+
 
 
     }
